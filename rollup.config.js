838d1a3a6533c7219524cc2f3710fdd802790d2a
UpperCod <mtrujillo@outlook.cl>
2021-05-05 01:09:43 -0400

    publish

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,11 @@
-import builtins from "builtin-modules";
-import renameExtensions from "@betit/rollup-plugin-rename-extensions";
 import pkg from "./package.json";
 
 export default {
-    input: ["./src/module.js"],
-    external: Object.keys(pkg.dependencies || {}).concat(builtins),
+    input: pkg.module,
+    external: Object.keys(pkg.dependencies || {}),
     output: {
-        dir: "./",
+        file: pkg.main,
         format: "cjs",
         sourcemap: true,
     },
-    plugins: [
-        renameExtensions({
-            include: ["**/*.js"],
-            mappings: {
-                ".js": ".cjs",
-            },
-        }),
-    ],
 };
