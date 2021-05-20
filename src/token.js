5b5faeade044adcf9470a2e1dc5566812ce9cc30
UpperCod <mtrujillo@outlook.cl>
2021-05-20 00:49:08 -0400

    It is improved in es-module-lexer analysis, allowing to completely replace the diamic import

--- src/token.js
+++ src/token.js
@@ -5,36 +5,55 @@ export class Token {
      */
     constructor(code, part) {
         this.src = code.slice(part.s, part.e);
-        this.input = code.slice(part.ss, part.se);
+        this.dinamic = part.d > -1;
+        this.mark = this.dinamic ? [part.ss, part.e + 1] : [part.ss, part.se];
+        this.input = code.slice(...this.mark);
         this.parse(this.input || this.src);
-        this.mark = this.type ? [part.ss, part.se] : [part.s, part.e];
     }
+
     /**
      *  @param {string} input
      */
     parse(input) {
         try {
-            const [, type, scope = "", quote = ""] = input.match(
-                /(\w+)\s*(?:(.+)\s*from){0,1}\s*("|')/s
-            );
-            this.scope = scope.trim();
-            this.quote = quote;
-            this.type = type == "import" || type == "export" ? type : "";
-            if (!this.type) {
+            if (this.dinamic) {
+                const [, quote] =
+                    input
+                        .replace(/\/\/(.*)/gm, "")
+                        .replace(/\s/g, "")
+                        .replace(/\/\*+([^*]*)\*+\//g, "")
+                        .match(/^import\(('|"|`)/) || [];
+                this.quote = quote;
                 this.src =
                     this.src[0] == this.quote
                         ? this.src.slice(1, this.src.length - 1)
                         : this.src;
+            } else {
+                const [, type, scope = "", quote = ""] = input.match(
+                    /(\w+)\s*(?:(.+)\s*from){0,1}\s*("|'|`)/s
+                );
+                this.scope = scope.trim();
+                this.quote = quote;
+                this.type = type == "import" || type == "export" ? type : "";
             }
         } catch (e) {
             this.toString = () => input;
         }
     }
     toString() {
-        const { type, scope, src, quote } = this;
-        return (
-            (type ? type + " " + (scope ? scope + " from " : "") : "") +
-            (quote + src + quote)
-        );
+        let { type, scope, src, quote } = this;
+        quote = quote || "";
+        if (type) {
+            return (
+                type +
+                " " +
+                (scope ? scope + " from " : "") +
+                quote +
+                src +
+                quote
+            );
+        } else {
+            return `import(${quote + src + quote})`;
+        }
     }
 }
